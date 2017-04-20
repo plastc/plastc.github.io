3cf05c37e20e3f3beb39c365c6190939aef9ec66
plastc-eng <eng@plastc.com>
2017-04-20 18:10:17 -0400

    letter

---
+++ Plastc_files/env.js
@@ -0,0 +1,6 @@
+window.plastc={};
+window.plastc['DEV']=false;
+window.plastc['FC_API_URL']='https://freecard.plastc.com/api';
+window.plastc['SUPPORT_API_URL']='https://website-support.plastc.com/api';
+window.plastc['RAVEN_DSN']='https://d2d0d534ad2a4ee39a637fd8f4269b0a@app.getsentry.com/31296';
+window.plastc['CBB_API_URL']='https://cbb.plastc.com/api';
